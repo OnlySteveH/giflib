04acac2e25e2cde76b92d6c2d3a2b762db5129ac
OnlySteveH <steve.hunter@bigdogconsultants.co.uk>
2016-01-27 17:19:42 +0000

    Adding a Category model

---
+++ main/java/uk/co/bigdogconsultants/giflib/model/Category.java
@@ -0,0 +1,30 @@
+package uk.co.bigdogconsultants.giflib.model;
+
+/**
+ * Created by Steve on 27/01/2016.
+ */
+public class Category {
+    private int id;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Category(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
